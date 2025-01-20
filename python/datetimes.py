1c536a4893be69beac9534c1fd00443b9543fc6d
Ajjayya G V <ajjayya2002@gmail.com>
2025-01-20 09:08:43 +0530

    date time

--- python/datetimes.py
+++ python/datetimes.py
@@ -1,6 +1,16 @@
-
 import datetime
 
 x = datetime.datetime.now()
 print(x)
 
+'''
+Creating Date Objects
+
+To create a  date , we can  use the datetime() class constructot of  the datetime module.
+
+the datetime() class requires three parametes to create a  date: year, month, day,
+
+'''
+
+y = datetime.datetime(2020,5,17)
+print(y)
