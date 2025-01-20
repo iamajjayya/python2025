5e880789c63148f890eb947351554781a1250905
Ajjayya G V <ajjayya2002@gmail.com>
2025-01-20 09:36:12 +0530

    Math concepts

---
+++ python/Math.py
@@ -0,0 +1,41 @@
+'''
+Python has  built un  math functions , including an extensive  math module, that allows  you to perform mathemmatical tasks on  Numbers 
+'''
+
+'''
+Buit in Math Functions
+
+'''
+
+x = min(5,10,25)
+y = max(5,10,25)
+
+print(x)
+print(y)
+
+z = abs(-7.25) # positive Value
+print(z)
+
+#pow  (x,y) function returns the value of  x  to power of y  (Xy)
+# example  4 to th  power  of  3  ( 4*4*4)
+
+x  = pow(4,3)
+print(x)
+
+
+import  math
+
+sq  = math.sqrt(64)  #sqrt
+ce = math.ceil(1.4)  #ceil
+fl = math.floor(1.4) #floor
+
+print(sq)
+print(ce)
+print(fl)
+
+
+# math.pi contsant , return the value of  PI (3.14)
+
+x  = math.pi
+print(x)
+
